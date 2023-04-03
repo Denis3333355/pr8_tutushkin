bcbc1e6c9845fe3c67cfce994975116a7d598ee1
Danil <danilrus58@yandex.ru>
2023-04-03 20:32:05 +0300

    Добавили верхний регистр main.py

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import random
 
 def password():
 k = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+a = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 rez = ''
 for i in range(11):
-rez += random.choice(k)
+rez += random.choice(k+a)
 return rez
 
 d = password()
